d5ab4aa1e88c27ad2baf129526d5a8818b734d48
David Arrozaqi <85738002+David4rr@users.noreply.github.com>
2026-08-02 02:39:43 +0700

    fix: improve performance change to image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio.example.com',
+  image: {
+    domains: ['images.unsplash.com'],
+  },
   vite: {
     plugins: [tailwindcss()]
   },
