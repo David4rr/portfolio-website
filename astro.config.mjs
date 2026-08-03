d8eb47c08ea0e8da1465286e23b346ff04ac2d26
David Arrozaqi <85738002+David4rr@users.noreply.github.com>
2026-08-03 11:14:38 +0700

    fix: bug cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,17 @@ import sitemap from '@astrojs/sitemap';
 
 import preact from '@astrojs/preact';
 import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
+
+const isDev = process.env.npm_lifecycle_event === 'dev';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio.example.com',
   output: 'server',
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-  }),
+  adapter: isDev 
+    ? node({ mode: 'standalone' }) 
+    : cloudflare({ imageService: 'cloudflare' }),
   image: {
     domains: ['images.unsplash.com'],
   },
